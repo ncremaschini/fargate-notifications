0060c5cce5df695baba8c4a415c6c6beecc97af0
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2024-03-03 22:43:49 +0100

    fix cleanup script output

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,9 +1,14 @@
+deleted_queues=0
+deleted_alarms=0
+deleted_sns_subscriptions=0
+
 queues=$(aws sqs list-queues --profile $PROFILE)
 for q in ${queues[@]}
 do
   if [ $q != "QUEUEURLS" ] && [ $q != "None" ]; then
     aws sqs delete-queue --queue-url $q --profile $PROFILE
     echo "deleted $q"
+    ((deleted_queues++))
   fi
 done
 
@@ -12,6 +17,7 @@ for alarm in ${alarms[@]}
 do
   aws cloudwatch delete-alarms --alarm-names $alarm --profile $PROFILE
   echo "Deleted alarm: $alarm"
+  ((deleted_alarms++))
 done
 
 sns_subscriptions=$(aws sns list-subscriptions --profile $PROFILE --query 'Subscriptions[?TopicArn==`arn:aws:sns:REGION:ACCOUNT_ID:fgtn-status-change`].SubscriptionArn' --output text)
@@ -19,12 +25,13 @@ for subscription in ${sns_subscriptions[@]}
 do
   aws sns unsubscribe --subscription-arn $subscription --profile $PROFILE
   echo "Deleted subscription: $subscription"
+  ((deleted_sns_subscriptions++))
 done
 
-#echo the number of deleted queues, alarms, and sns subscriptions
-echo "Deleted $(echo $queues | wc -w) queues"
-echo "Deleted $(echo $alarms | wc -w) alarms"
-echo "Deleted $(echo $sns_subscriptions | wc -w) sns subscriptions"
+echo "Deleted $deleted_queues queues"
+echo "Deleted $deleted_alarms alarms"
+echo "Deleted $deleted_sns_subscriptions sns subscriptions"
+
 
 
 
