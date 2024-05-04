c7bb5a0374d66db0dc3d3ca4e45aaf3f2d1fcde1
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2024-05-04 14:57:41 +0200

    remove event bridge useless custom metrics and add delete rule command on tear down

--- lib/metrics.ts
+++ lib/metrics.ts
@@ -59,14 +59,4 @@ export function createSqsProcessorTimetakenMetric(
     defaultValue: 0,
     unit: cloudwatch.Unit.COUNT,
   });
-
-  new logs.MetricFilter(stack, "fgnt-sqs-processor-metric-filter-6", {
-    logGroup: appProps.eventProcessorLogGroup!,
-    metricNamespace: "Fgnt",
-    metricName: "eventBridgeTimeTaken",
-    filterPattern: logs.FilterPattern.exists("$.eventBridgeTimeTakenInMillis"),
-    metricValue: "$.eventBridgeTimeTakenInMillis",
-    defaultValue: 0,
-    unit: cloudwatch.Unit.MILLISECONDS,
-  });
 }
